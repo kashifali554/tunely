e520aa8c05d649f38e69010f3ac7bc949a6fed2e
kashifali554 <kashifali874@gmail.com>
2017-03-09 19:40:20 -0800

    Sprint One finished

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/tunely");
 
-module.exports = {
-  api: require('./apiController'),
-  albums: require('./albumsController')
-} 
+// module.exports = {
+//   api: require('./apiController'),
+//   albums: require('./albumsController')
+// } 
+
+var Album = require('./album');
+
+module.exports.Album = Album;
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,46 +8,107 @@
  */
 
 /* hard-coded data! */
-var sampleAlbums = [];
-sampleAlbums.push({
-             artistName: 'Ladyhawke',
-             name: 'Ladyhawke',
-             releaseDate: '2008, November 18',
-             genres: [ 'new wave', 'indie rock', 'synth pop' ]
-           });
-sampleAlbums.push({
-             artistName: 'The Knife',
-             name: 'Silent Shout',
-             releaseDate: '2006, February 17',
-             genres: [ 'synth pop', 'electronica', 'experimental' ]
-           });
-sampleAlbums.push({
-             artistName: 'Juno Reactor',
-             name: 'Shango',
-             releaseDate: '2000, October 9',
-             genres: [ 'electronic', 'goa trance', 'tribal house' ]
-           });
-sampleAlbums.push({
-             artistName: 'Philip Wesley',
-             name: 'Dark Night of the Soul',
-             releaseDate: '2008, September 12',
-             genres: [ 'piano' ]
-           });
+// var sampleAlbums = [];
+// sampleAlbums.push({
+//              artistName: 'Ladyhawke',
+//              name: 'Ladyhawke',
+//              releaseDate: '2008, November 18',
+//              genres: [ 'new wave', 'indie rock', 'synth pop' ]
+//            });
+// sampleAlbums.push({
+//              artistName: 'The Knife',
+//              name: 'Silent Shout',
+//              releaseDate: '2006, February 17',
+//              genres: [ 'synth pop', 'electronica', 'experimental' ]
+//            });
+// sampleAlbums.push({
+//              artistName: 'Juno Reactor',
+//              name: 'Shango',
+//              releaseDate: '2000, October 9',
+//              genres: [ 'electronic', 'goa trance', 'tribal house' ]
+//            });
+// sampleAlbums.push({
+//              artistName: 'Philip Wesley',
+//              name: 'Dark Night of the Soul',
+//              releaseDate: '2008, September 12',
+//              genres: [ 'piano' ]
+//            });
 /* end of hard-coded data */
 
 
 
 $(document).ready(function() {
   console.log('app.js loaded!');
-  renderAlbum(sampleAlbums[0]);
+
+  // make a get request for all albums
+  $.ajax({
+    method: 'GET',
+    url: '/api/albums',
+    success: handleSuccess,
+    error: handleError
+  });
 });
 
+function handleSuccess (albumList) {
+    albumList.forEach(function(album) {
+      renderAlbum(album);
+    });
+};
 
-
-
-
-// this function takes a single album and renders it to the page
-function renderAlbum(album) {
-  console.log('rendering album:', album);
-
+function handleError(err){
+  console.log('There has been an error: ', err);
 }
+
+// this function takes in a single album and renders it to the page
+function renderAlbum(album) {
+  console.log('rendering album ', album);
+  var albumHtml = (`
+    <div class="row album">
+      <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default">
+          <div class="panel-body">
+          <!-- begin album internal row -->
+            <div class='row'>
+              <div class="col-md-3 col-xs-12 thumbnail album-art">
+                <img src="images/800x800.png" alt="album image">
+              </div>
+              <div class="col-md-9 col-xs-12">
+                <ul class="list-group">
+                  <li class="list-group-item">
+                    <h4 class='inline-header'>Album Name:</h4>
+                    <span class='album-name'>${album.name}</span>
+                  </li>
+                  <li class="list-group-item">
+                    <h4 class='inline-header'>Artist Name:</h4>
+                    <span class='artist-name'>${album.artistName}</span>
+                  </li>
+                  <li class="list-group-item">
+                    <h4 class='inline-header'>Released date:</h4>
+                    <span class='album-releaseDate'>${album.releaseDate}</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <!-- end of album internal row -->
+            <div class='panel-footer'>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end one album -->
+  `);
+  $('#albums').append(albumHtml);
+}
+
+
+
+
+// // this function takes a single album and renders it to the page
+// function renderAlbum(album) {
+//   console.log('rendering album:', album);
+//   var albumHtml = $('#album-template').html();
+//   var albumsTemplate = Handlebars.compile(albumHtml);
+//   var html = albumsTemplate(album);
+//   $('#albums').append(html);
+// }
